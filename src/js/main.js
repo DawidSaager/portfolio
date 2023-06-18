c234aab45f36deffe8a7964c246735b4cddd43c7
Dawid <dawidsaager@gmail.com>
2023-06-18 22:15:38 +0200

    add statistics section

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,11 @@ const burger = document.querySelector('.burger-btn');
 const nav = document.querySelector('.nav__items');
 const navItem = document.querySelectorAll('.nav__item');
 const headerBTN = document.querySelector('.header__button')
-const aboutSection = document.querySelector('.about')
+const offersSection = document.querySelector('.offers')
+
+const counterItems = document.querySelectorAll('.statistics__counter')
+const counterBox = document.querySelector('.statistics__counter-box')
+
 
 const openNavMobile = () => {
 	nav.classList.toggle('nav__items--active');
@@ -15,9 +19,45 @@ const openNavMobile = () => {
 };
 
 const clickHeaderBTN = () => {
-	window.scrollBy(0, aboutSection.offsetTop - window.scrollY)
+	window.scrollBy(0, offersSection.offsetTop - window.scrollY)
 }
 
+const options = {
+	rootMargin: '-150px'
+}
+
+const startCounter = entry => {
+	console.log(entry[0].isIntersecting);
+
+	if(entry[0].isIntersecting){
+		counterItems.forEach(counter => {
+
+
+
+			const updateCounter = () => {
+				const finalNumber = counter.getAttribute('data-number')
+				const value = parseInt(counter.textContent)
+
+				const speed = finalNumber / 10
+
+				if(value < finalNumber){
+					counter.textContent = `${Math.floor(value + speed)}`
+					setTimeout(updateCounter, 100)
+				} else{
+					counter.textContent = finalNumber
+				}
+			}
+
+			updateCounter()
+
+
+		})
+	}
+}
+
+
+const observer = new IntersectionObserver(startCounter, options)
+observer.observe(counterBox)
 
 burger.addEventListener('click', openNavMobile);
 headerBTN.addEventListener('click', clickHeaderBTN);
